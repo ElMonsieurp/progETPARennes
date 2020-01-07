105ab90e97363f287958e6fe34694358c98385de
SeleneTononETPA <59515324+SeleneTononETPA@users.noreply.github.com>
2020-01-07 14:16:44 +0100

    2 variables + conditionnel
    Un autre commit où je change les variables et j'ajoute une condition

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-var uneVariable="23 et 34";
-let uneAutreVariable = 23;
+var une ="23 et 34";
+let uneAutre = 23;
 
+// ici c'est du commentaire
+// encore du commentaire
+
+if (une!= uneAutre) {
+}
